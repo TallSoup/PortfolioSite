edd03ba9d8a1a3000ac7c0558669d3e0dcdc8a09
Andrew <andrewtclarkson@gmail.com>
2022-01-26 19:36:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Portfolio Website
+
+My first (and definitely not last) take on my portfolio. This project made me realize I'd like to learn more front end - it feels good to make things that look nice!
+
+I used Flask for the backend, built on top of a Bootstrapmade template with some tweaks, with a SQLAlchemy/PostgreSQL database for my projects (yes, total overkill). 
+
+**Next iteration will be more customized.**
+
+I used formspree to handle the contact form, I will be returning to this once I learn some javascript. Http requests require a reload of the page, and given this is a one page site, that means a scroll back to the top, and you miss my nice little flashed messages.  There's some commented out code in there I kept for this.
+
+Thanks for looking!
---
+++ main.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import String, Column, Integer
+import os
+import datetime as dt
+from sqlalchemy.sql.expression import func
+from wtforms import Form, StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email
+from flask_wtf import FlaskForm
+import smtplib
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+
+# ckeditor = CKEditor(app)
+Bootstrap(app)
+
+# CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///portdb1.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Portfolio(db.Model):
+    id = Column(Integer, primary_key=True)
+    filter = Column(String(120), nullable=False)
+    img = Column(String(500), nullable=False)
+    alt = Column(String(500), nullable=True)
+    h4_desc_title = Column(String(250), nullable=False)
+    category = Column(String(500), nullable=False)
+    lightbox_desc = Column(String(500), nullable=False)
+    link = Column(String(500), nullable=False)
+
+# class ContactForm(FlaskForm):
+#     name = StringField("Name", validators=[DataRequired()])
+#     email = StringField("Email", validators=[DataRequired(), Email()])
+#     message = TextAreaField("Message", validators=[DataRequired()], render_kw={"rows": 10})
+#     submit = SubmitField("Send")
+
+
+# creates the table and database
+db.create_all()
+
+
+# def send_email(name, email, message):
+#     with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+#         connection.starttls()
+#         connection.login(user=os.environ.get("FROM_EMAIL"), password=os.environ.get("FROM_PASSWORD"))
+#         connection.sendmail(
+#             from_addr=os.environ.get("FROM_EMAIL"),
+#             to_addrs=os.environ.get("MY_EMAIL"),
+#             msg=f"Subject:New Email from Portfolio Site\n\nFrom: {name}, Email: {email}, Message: {message}")
+
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    all_portfolio = db.session.query(Portfolio).order_by(func.random())
+    filters = list(set([x.filter for x in all_portfolio if x.filter]))
+    # form = ContactForm()
+    # if form.validate_on_submit():
+    #     name = form.name.data
+    #     email = form.email.data
+    #     message = form.message.data
+    #     send_email(name, email, message)
+    #     flash("Thanks for the message! I'll be in touch soon!")
+    #     return redirect(url_for('home'))
+    return render_template('index.html', portfolio=all_portfolio, filters=filters)
+
+
+# footer year
+@app.context_processor
+def inject_year():
+    year = dt.datetime.now().strftime("%Y")
+    return {"year": year}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
